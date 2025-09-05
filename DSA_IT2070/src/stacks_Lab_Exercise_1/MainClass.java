d5bb11cf74d3da4898a4e86bd26a44dfdf9eb043
Supun Kalhara Jayasinghe <rmskjayasinghe2025@gmail.com>
2025-09-05 12:54:49 +0530

    Comment on the order of insertion to the stack and the order of removal from the stack.

--- DSA_IT2070/src/stacks_Lab_Exercise_1/MainClass.java
+++ DSA_IT2070/src/stacks_Lab_Exercise_1/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
-		CharStack stack = new CharStack(5);
+		Stack stack = new Stack(5);
 		
 		stack.push('A');
 		stack.push('B');
@@ -14,9 +14,12 @@ public class MainClass {
 		
 		System.out.println("Removing item from stack");
 		while(!stack.isFull()) {
-			System.out.println(stack.pop);
+			System.out.println(stack.pop());
 		}
 		
-		
+		System.out.println("\nObservation:");
+        System.out.println("Elements inserted in order: A, B, C, D, E");
+        System.out.println("Elements removed in order: E, D, C, B, A");
+        System.out.println("=> This shows that Stack follows LIFO (Last In First Out).");
 	}
 }
