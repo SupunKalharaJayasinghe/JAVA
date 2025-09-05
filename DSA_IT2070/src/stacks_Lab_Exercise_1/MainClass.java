c0cb6431cc95b9861723e185985d21b9ab49b4b3
Supun Kalhara Jayasinghe <rmskjayasinghe2025@gmail.com>
2025-09-05 12:41:13 +0530

    Create main class  and Insert characters to the stack using push() method.

---
+++ DSA_IT2070/src/stacks_Lab_Exercise_1/MainClass.java
@@ -0,0 +1,16 @@
+package stacks_Lab_Exercise_1;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		CharStack stack = new CharStack(5);
+		
+		stack.push('A');
+		stack.push('B');
+		stack.push('C');
+		stack.push('D');
+		stack.push('E');
+		
+	}
+}
